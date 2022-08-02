d0b788b721596b2b51f35ffb3bd26bbee0443843
RobertCox972 <107111535+RobertCox972@users.noreply.github.com>
2022-08-02 12:50:06 +0100

    removes unwanted console logs from controllers file

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -4,7 +4,6 @@ const { selectTopics } = require('../models/app.models')
 
 exports.getTopics = (req, res) => {
 selectTopics().then((topics) => {
-    //console.log(topics)
     res.status(200).send({ topics: topics})
 })
 }
--- models/app.models.js
+++ models/app.models.js
@@ -4,6 +4,6 @@ exports.selectTopics = () => {
     return db 
     .query("SELECT * FROM topics;")
     .then(({ rows: topics }) => {
-    return topics;
+        return topics;
     })
 }
